88b7be6117ca8ad8edfe1a5167531257ecbd2c5b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-09-12 12:03:20 +0700

    chore: configure shadcn

---
+++ src/styles/fonts.ts
@@ -0,0 +1,15 @@
+import {
+  JetBrains_Mono as FontMono,
+  Inter as FontSans,
+} from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  variable: "--font-mono",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
